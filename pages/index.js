0adf420ef2c6176924c8e3ab5e812783b36e175d
MitoruDev <elmascaner2005@gmail.com>
2023-07-29 18:03:42 +0200

    Adding Navbar

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import {BsFillMoonStarsFill} from 'react-icons/bs'
 
 export default function Home() {
   return (
@@ -10,8 +11,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <section className=""></section>
+      <main className=" bg-white px-10">
+        <section className="min-h-screen">
+    	    <nav className="py-10 mb-12 flex justify-between">
+           <h1 className="text-xl">Mitoru Portfolio</h1> 
+           <ul className="flex items-center">
+              <li><BsFillMoonStarsFill className="cursor-pointer text-2xl"/></li>
+              <li><a className=" bg-gradient-to-r from-cyan-500 to-sky-500 text-white px-4 py-2 rounded ml-8" href="#">Lebenslauf</a></li>
+           </ul>
+          </nav>
+        </section>
       </main>
     </>
   );
